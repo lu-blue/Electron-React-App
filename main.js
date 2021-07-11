4d838bfd17c75a3e78640648baa51c5543180953
Lubov <lubov,novozhilova@gmail.com>
2021-07-11 17:58:49 +0200

    Added package json, and simple index html ad main js

---
+++ main.js
@@ -0,0 +1,20 @@
+const { BrowserWindow, app } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    backgroundColor: "white",
+    webPreferences: {
+      nodeIntegration: false,
+      worldSafeExecuteJavaScript: true,
+      contextIsolation: true,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
